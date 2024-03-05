3e49e49cc2649d267a2a1df9c78f05b4e2122541
omar <omarmuhammad0115@gmail.com>
2024-03-05 16:02:39 +0200

    Adding youtube header UI (no functionalty yet)

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export const metadata = {
+    title: "digital life manger",
+    description: 
+    "digital life manger to limit the time spent in social media and youtube, for better performance in life",
+}
+
+export default function App() {
+
+    return (
+        <div className="home">
+            <h1 className="home-h1">Home page</h1>
+            <p className="home-p">Coming soon...</p>
+        </div>   
+    )
+}
+
---
+++ app/youtube/page.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+
+export const metadata = {
+    title: "Youtube",
+    description: "youtube clone to manage time spent in youtube, time and video limit",
+}
+
+export default function Youtube() {
+
+    return (
+        <>
+            <section>
+                <Header />
+            </section>
+        </>
+    )
+}
